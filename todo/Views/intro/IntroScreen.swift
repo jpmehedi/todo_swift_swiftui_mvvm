b716ab9082de489292642fedd015d7388f5bf3f9
jpmehedi <mehedihasan.cse.jp@gmail.com>
2025-02-03 13:16:28 +0900

    Chips ui create

--- todo/Views/intro/IntroScreen.swift
+++ todo/Views/intro/IntroScreen.swift
@@ -16,23 +16,58 @@ struct IntroScreen: View {
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
-            Button(action:{
-                print("Clickd me")
-            }){
-                Text("Continue")
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.black)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    
-                    
+            
+            VStack(
+                alignment: .center
+            ){
+                Spacer()
+                Text("Pick some new\nhabits to get started")
+                    .font(.title)
+                    .fontWeight(.black)
+                    .bold()
+                    .lineSpacing(10)
+                Spacer()
+                HStack{
+                    Text("Excecise")
+                        .padding(.horizontal, 24)
+                        .padding(.vertical, 10)
+                        .background(Color.gray.opacity(0.1))
+                        .foregroundColor(.black)
+                        .clipShape(CustomRoundedShape(cornerRadius: 8))
+                        .shadow(radius: 5)
+                }
+                Button(action:{
+                    print("Clickd me")
+                }){
+                    Text("Continue")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        
+                        
+                }
+                .padding(30)
             }
-            .padding(30)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color("#F9F5F4"))
+
         }
     }
 }
 
+
 #Preview {
     IntroScreen()
+}
+
+
+struct CustomRoundedShape : Shape{
+    var cornerRadius : CGFloat
+    func path(in rect: CGRect) -> Path {
+        var path = Path();
+        path.addRoundedRect(in: rect, cornerSize: CGSize(width: cornerRadius, height: cornerRadius))
+        return path
+    }
 }
