16274def91b2fb570e3ed04a25972814d68408a1
jpmehedi <mehedihasan.cse.jp@gmail.com>
2025-02-07 15:49:54 +0900

    CheckBox design, TextField design, Home Screen design

--- todo/Views/Home/HomeScreen.swift
+++ todo/Views/Home/HomeScreen.swift
@@ -8,16 +8,78 @@
 import SwiftUI
 
 struct HomeScreen : View{
-    @ObservedObject var appText = AppText()
-    var body : some View{
-        NavigationView{
-            Text("Hello")
+    @State var isChecked = false
+    @State private var text = ""
+    var body: some View{
+        VStack(
+            alignment: .leading
+        ){
+            Text("Today")
+                .foregroundColor(Color(hex: 0x000000))
+                .padding(.bottom, 0)
+                .font(.system(size: 36))
+                .fontWeight(.bold)
+            Spacer()
+            VStack(
+                alignment: .leading
+            ){
+                Text("Design")
+                    .foregroundColor(Color(hex: 0xD1A28B))
+                    .textCase(.uppercase)
+                    .padding(.bottom, 0)
+                    .font(.system(size: 13))
+                
+                
+                CheckBox(
+                    isChecked: isChecked,
+                    title: "Read a chapter of a book",
+                    onTap: {
+                        isChecked.toggle()
+                    }
+                )
+                CheckBox(
+                    isChecked: isChecked,
+                    title: "Review daily goals before sleeping. Add some new if time permits",
+                    onTap: {
+                        isChecked.toggle()
+                    }
+                )
+            }
+            Spacer()
             
+            HStack(
+                alignment: .center
+            ){
+                TextField("Write a task...", text: $text)
+                    .padding()
+                    .background(Color(hex: 0xF3EFEE))
+                    .cornerRadius(8)
+                    .frame(height: 56)
+                
+                Button(action:{
+                    print("Clickd me")
+                }){
+                    Text("Add")
+                        .padding()
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .padding()
+            }
+
+    
         }
-        .navigationTitle(appText.homeScreenTitle)
+        .padding(.all)
+
     }
 }
 
 #Preview {
     HomeScreen()
 }
+
+
+
+
+
